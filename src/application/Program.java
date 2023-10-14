12bcbb5b89192d004b4d88528b21bf2dbbac84bc
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-10-14 10:04:01 -0300

    Ready-made external file reading processing structure

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,32 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import model.entities.Canditate;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		Map<Canditate, Integer> totalVotes = new HashMap<Canditate, Integer>();
+		
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		sc.close();
 
 	}
 
